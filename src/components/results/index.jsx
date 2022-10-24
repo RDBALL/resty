9b17b0e208294e89d7b55b623c1419f13c47956e
Robert Ball <bobball89@gmail.com>
2022-10-24 22:29:18 +0000

    updated results class to function

---
+++ src/components/results/index.jsx
@@ -0,0 +1,8 @@
+function Results(props){
+    return (
+      <section>
+        <pre>{props.data ? JSON.stringify(props.data, undefined, 2) : null}</pre>
+      </section>
+    );
+  }
+export default Results;
