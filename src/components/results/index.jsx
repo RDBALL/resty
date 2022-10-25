5342b5c4b2dad684930ffcc084e15f61208f7934
Robert Ball <bobball89@gmail.com>
2022-10-25 22:19:04 +0000

    added JSON theme to results

--- src/components/results/index.jsx
+++ src/components/results/index.jsx
@@ -1,10 +1,19 @@
 import './style.scss';
+import JSONPretty from 'react-json-pretty';
+import 'react-json-pretty/themes/1337.css';
 
-const Results = ({data}) => {
-    return (
-      <section>
-        <pre>{data ? JSON.stringify(data, undefined, 2) : null}</pre>
-      </section>
-    );
-  }
+const Results = ({ data }) => {
+  return (
+    <section>
+      {data ?
+        (<>
+          <span>Results
+          <JSONPretty data={data}/>
+          </span>
+        </>) :
+        (<span>Awaiting API Request</span>)
+      }
+    </section>
+  );
+}
 export default Results;
